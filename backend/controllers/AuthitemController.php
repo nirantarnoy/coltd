1da9917f8b863c07ff0f1cb035c22e92926276be
tarlektarnoy <niran_tarlek@hotmail.com>
2019-01-16 20:32:20 +0700

    'update'

--- backend/controllers/AuthitemController.php
+++ backend/controllers/AuthitemController.php
@@ -603,8 +603,8 @@ class AuthitemController extends Controller
         $auth->addChild($user_role,$manage_productionrec);
 
 
-        $auth->assign($admin_role,3);
-        $auth->assign($user_role,3);
+        $auth->assign($admin_role,1);
+        $auth->assign($user_role,1);
 
 
 
