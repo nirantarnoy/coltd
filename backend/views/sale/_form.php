11cbda0b4d1b287a2ac4efb5a7e5d9dbdd449380
tarlektarnoy <niran_tarlek@hotmail.com>
2019-05-31 20:33:10 +0700

    'update'

--- backend/views/sale/_form.php
+++ backend/views/sale/_form.php
@@ -295,7 +295,7 @@ $this->registerCss('
         <div class="panel-group" id="accordion">
             <ul class="nav nav-tabs">
                 <li class="active"><a data-toggle="tab" href="#home">รายการ</a></li>
-                <li><a data-toggle="tab" href="#menu1">รายละอียด</a></li>
+<!--                <li><a data-toggle="tab" href="#menu1">รายละอียด</a></li>-->
             </ul>
 
             <div class="tab-content">
