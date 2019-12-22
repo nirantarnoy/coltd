c37d47a3d360379bce7545bc001331c894bf5dd8
tarlektarnoy <niran_tarlek@hotmail.com>
2019-12-22 19:24:34 +0700

    'update'

--- backend/views/quotation/index.php
+++ backend/views/quotation/index.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
 use yii\helpers\Url;
+
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\QuotationSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -12,13 +13,15 @@ $this->title = 'เสนอราคา';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="quotation-index">
-    <?php Pjax::begin();?>
+    <?php Pjax::begin(); ?>
     <div class="panel panel-headline">
         <div class="panel-heading">
             <div class="btn-group">
                 <?= Html::a(Yii::t('app', '<i class="fa fa-plus"></i> สร้างใบเสนอราคา'), ['create'], ['class' => 'btn btn-success']) ?>
             </div>
-            <h4 class="pull-right"><?=$this->title?> <i class="fa fa-question-circle-o"></i><small></small></h4>
+            <h4 class="pull-right"><?= $this->title ?> <i class="fa fa-question-circle-o"></i>
+                <small></small>
+            </h4>
             <!-- <ul class="nav navbar-right panel_toolbox">
               <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
               <li class="dropdown">
@@ -39,18 +42,19 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="row">
                 <div class="col-lg-9">
                     <div class="form-inline">
-                        <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+                        <?php echo $this->render('_search', ['model' => $searchModel]); ?>
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="pull-right">
-                        <form id="form-perpage" class="form-inline" action="<?=Url::to(['quotation/index'],true)?>" method="post">
+                        <form id="form-perpage" class="form-inline" action="<?= Url::to(['quotation/index'], true) ?>"
+                              method="post">
                             <div class="form-group">
                                 <label>แสดง </label>
                                 <select class="form-control" name="perpage" id="perpage">
-                                    <option value="20" <?=$perpage=='20'?'selected':''?>>20</option>
-                                    <option value="50" <?=$perpage=='50'?'selected':''?> >50</option>
-                                    <option value="100" <?=$perpage=='100'?'selected':''?>>100</option>
+                                    <option value="20" <?= $perpage == '20' ? 'selected' : '' ?>>20</option>
+                                    <option value="50" <?= $perpage == '50' ? 'selected' : '' ?> >50</option>
+                                    <option value="100" <?= $perpage == '100' ? 'selected' : '' ?>>100</option>
                                 </select>
                                 <label> รายการ</label>
                             </div>
@@ -59,116 +63,116 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
             </div>
             <div class="table-responsive">
+               
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    //'filterModel' => $searchModel,
+                    'emptyCell' => '-',
+                    'layout' => "{items}\n{summary}\n<div class='text-center'>{pager}</div>",
+                    'summary' => "แสดง {begin} - {end} ของทั้งหมด {totalCount} รายการ",
+                    'showOnEmpty' => false,
+                    'tableOptions' => ['class' => 'table table-hover'],
+                    'emptyText' => '<br /><div style="color: red;align: center;"> <b>ไม่พบรายการไดๆ</b></div>',
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        //'filterModel' => $searchModel,
-        'emptyCell'=>'-',
-        'layout'=>"{items}\n{summary}\n<div class='text-center'>{pager}</div>",
-        'summary' => "แสดง {begin} - {end} ของทั้งหมด {totalCount} รายการ",
-        'showOnEmpty'=>false,
-        'tableOptions' => ['class' => 'table table-hover'],
-        'emptyText' => '<br /><div style="color: red;align: center;"> <b>ไม่พบรายการไดๆ</b></div>',
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                        //'id',
+                        'quotation_no',
+                        //'revise',
+                        [
+                            'attribute' => 'require_date',
+                            'value' => function ($data) {
+                                return date('d/m/Y', $data->require_date);
+                            }
+                        ],
+                        [
+                            'attribute' => 'customer_id',
+                            'value' => function ($data) {
+                                return \backend\models\Customer::findName($data->customer_id);
+                            }
+                        ],
+                        [
+                            'attribute' => 'status',
+                            'format' => 'raw',
+                            'value' => function ($data) {
+                                if ($data->status == 1) {
+                                    return "<div class='label label-success'> Open</div>";
+                                } else if ($data->status == 2) {
+                                    return "<div class='label label-danger'> Completed</div>";
+                                }
+                            }
+                        ],
+                        //'customer_ref',
+                        //'delvery_to',
+                        //'currency',
+                        //'sale_id',
+                        //'disc_amount',
+                        //'disc_percent',
+                        //'total_amount',
+                        //'note',
+                        //'status',
+                        //'created_at',
+                        //'updated_at',
+                        //'created_by',
+                        //'updated_by',
 
-            //'id',
-            'quotation_no',
-            //'revise',
-            [
-                'attribute'=>'require_date',
-                'value' => function($data){
-                    return date('d/m/Y',$data->require_date);
-                }
-            ],
-            [
-                'attribute'=>'customer_id',
-                'value' => function($data){
-                    return \backend\models\Customer::findName($data->customer_id);
-                }
-            ],
-            [
-                'attribute'=>'status',
-                'format' => 'raw',
-                'value' => function($data){
-                    if($data->status == 1){
-                        return "<div class='label label-success'> Open</div>";
-                    }else if($data->status == 2){
-                        return "<div class='label label-danger'> Completed</div>";
-                    }
-                }
-            ],
-            //'customer_ref',
-            //'delvery_to',
-            //'currency',
-            //'sale_id',
-            //'disc_amount',
-            //'disc_percent',
-            //'total_amount',
-            //'note',
-            //'status',
-            //'created_at',
-            //'updated_at',
-            //'created_by',
-            //'updated_by',
+                        [
 
-            [
-
-                'header' => '',
-                'headerOptions' => ['style' => 'text-align:center;','class' => 'activity-view-link',],
-                'class' => 'yii\grid\ActionColumn',
-                'contentOptions' => ['style' => 'text-align: right'],
-                'buttons' => [
-                    'view' => function($url, $data, $index) {
-                        $options = [
-                            'title' => Yii::t('yii', 'View'),
-                            'aria-label' => Yii::t('yii', 'View'),
-                            'data-pjax' => '0',
-                        ];
-                        return Html::a(
-                            '<span class="glyphicon glyphicon-eye-open btn btn-xs btn-default"></span>', $url, $options);
-                    },
-                    'update' => function($url, $data, $index) {
-                        $options = array_merge([
-                            'title' => Yii::t('yii', 'Update'),
-                            'aria-label' => Yii::t('yii', 'Update'),
-                            'data-pjax' => '0',
-                            'id'=>'modaledit',
-                        ]);
-                        return Html::a(
-                            '<span class="glyphicon glyphicon-pencil btn btn-xs btn-default"></span>', $url, [
-                            'id' => 'activity-view-link',
-                            //'data-toggle' => 'modal',
-                            // 'data-target' => '#modal',
-                            'data-id' => $index,
-                            'data-pjax' => '0',
-                            // 'style'=>['float'=>'rigth'],
-                        ]);
-                    },
-                    'delete' => function($url, $data, $index) {
-                        $options = array_merge([
-                            'title' => Yii::t('yii', 'Delete'),
-                            'aria-label' => Yii::t('yii', 'Delete'),
-                            //'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                            //'data-method' => 'post',
-                            //'data-pjax' => '0',
-                            'data-url'=>$url,
-                            'onclick'=>'recDelete($(this));'
-                        ]);
-                        return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-default"></span>', 'javascript:void(0)', $options);
-                    }
-                ]
-            ],
-        ],
-    ]); ?>
-    <?php Pjax::end(); ?>
+                            'header' => '',
+                            'headerOptions' => ['style' => 'text-align:center;', 'class' => 'activity-view-link',],
+                            'class' => 'yii\grid\ActionColumn',
+                            'contentOptions' => ['style' => 'text-align: right'],
+                            'buttons' => [
+                                'view' => function ($url, $data, $index) {
+                                    $options = [
+                                        'title' => Yii::t('yii', 'View'),
+                                        'aria-label' => Yii::t('yii', 'View'),
+                                        'data-pjax' => '0',
+                                    ];
+                                    return Html::a(
+                                        '<span class="glyphicon glyphicon-eye-open btn btn-xs btn-default"></span>', $url, $options);
+                                },
+                                'update' => function ($url, $data, $index) {
+                                    $options = array_merge([
+                                        'title' => Yii::t('yii', 'Update'),
+                                        'aria-label' => Yii::t('yii', 'Update'),
+                                        'data-pjax' => '0',
+                                        'id' => 'modaledit',
+                                    ]);
+                                    return Html::a(
+                                        '<span class="glyphicon glyphicon-pencil btn btn-xs btn-default"></span>', $url, [
+                                        'id' => 'activity-view-link',
+                                        //'data-toggle' => 'modal',
+                                        // 'data-target' => '#modal',
+                                        'data-id' => $index,
+                                        'data-pjax' => '0',
+                                        // 'style'=>['float'=>'rigth'],
+                                    ]);
+                                },
+                                'delete' => function ($url, $data, $index) {
+                                    $options = array_merge([
+                                        'title' => Yii::t('yii', 'Delete'),
+                                        'aria-label' => Yii::t('yii', 'Delete'),
+                                        //'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                                        //'data-method' => 'post',
+                                        //'data-pjax' => '0',
+                                        'data-url' => $url,
+                                        'onclick' => 'recDelete($(this));'
+                                    ]);
+                                    return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-default"></span>', 'javascript:void(0)', $options);
+                                }
+                            ]
+                        ],
+                    ],
+                ]); ?>
+                <?php Pjax::end(); ?>
             </div>
         </div>
     </div>
 </div>
 <?php
-$this->registerJsFile( '@web/js/sweetalert.min.js',['depends' => [\yii\web\JqueryAsset::className()]],static::POS_END);
-$this->registerCssFile( '@web/css/sweetalert.css');
+$this->registerJsFile('@web/js/sweetalert.min.js', ['depends' => [\yii\web\JqueryAsset::className()]], static::POS_END);
+$this->registerCssFile('@web/css/sweetalert.css');
 //$url_to_delete =  Url::to(['product/bulkdelete'],true);
 $this->registerJs('
     $(function(){
@@ -193,6 +197,6 @@ $this->registerJs('
         });
     }
 
-    ',static::POS_END);
+    ', static::POS_END);
 ?>
 
