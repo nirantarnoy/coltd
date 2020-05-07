56e456ac6b9a24c0800266cbf7d5769c701eb7a1
tarlektarnoy <niran_tarlek@hotmail.com>
2020-05-07 13:07:41 +0700

    'update'

--- backend/views/productstock/index.php
+++ backend/views/productstock/index.php
@@ -5,6 +5,7 @@ use kartik\grid\GridView;
 use yii\widgets\Pjax;
 use yii\helpers\Url;
 use kartik\export\ExportMenu;
+
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\ProductstockSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -16,7 +17,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="panel panel-headline">
         <div class="panel-heading">
-            <h4 class="pull-left"><i class="fa fa-cubes text-warning"></i><small></small> <?=$this->title?> </h4>
+            <h4 class="pull-left"><i class="fa fa-cubes text-warning"></i>
+                <small></small> <?= $this->title ?> </h4>
             <!-- <ul class="nav navbar-right panel_toolbox">
               <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
               <li class="dropdown">
@@ -45,47 +47,56 @@ $this->params['breadcrumbs'][] = $this->title;
                             ['class' => 'yii\grid\SerialColumn'],
 
                             // 'id',
-                            ['attribute' => 'product_id','value'=>function($data){
+                            ['attribute' => 'product_id', 'value' => function ($data) {
                                 return \backend\models\Product::findCode($data->product_id);
                             }],
-                            ['attribute' => 'product_id','value'=>function($data){
+                            ['attribute' => 'product_id', 'value' => function ($data) {
                                 return \backend\models\Product::findName($data->product_id);
                             }],
-                            ['attribute' => 'warehouse_id','value'=>function($data){
+                            ['attribute' => 'warehouse_id', 'value' => function ($data) {
                                 return \backend\models\Warehouse::findWarehousename($data->warehouse_id);
                             }],
                             [
                                 'attribute' => 'qty',
-                                'headerOptions' => ['style'=>'background-color: green;color: white'],
-                                'contentOptions' => ['style'=>'background-color: green;color: white;text-align: right'],
-                                'value'=>function($data){
-                                    return $data->qty!=null?$data->qty:0;
+                                'headerOptions' => ['style' => 'background-color: green;color: white'],
+                                'contentOptions' => ['style' => 'background-color: green;color: white;text-align: right'],
+                                'value' => function ($data) {
+                                    return $data->qty != null ? $data->qty : 0;
                                 }],
                             [
                                 'attribute' => 'out_qty',
-                                'headerOptions' => ['style'=>'background-color: orange;color: white'],
-                                'contentOptions' => ['style'=>'background-color: orange;color: white;text-align: right'],
-                                'value'=>function($data){
-                                    return $data->out_qty!=null?$data->out_qty:0;
+                                'headerOptions' => ['style' => 'background-color: orange;color: white'],
+                                'contentOptions' => ['style' => 'background-color: orange;color: white;text-align: right'],
+                                'value' => function ($data) {
+                                    return $data->out_qty != null ? $data->out_qty : 0;
                                 }],
-                            'invoice_no',
-                            ['attribute' => 'invoice_date','value'=>function($data){
-                                return date('d-m-Y',strtotime($data->invoice_date));
+                            [
+                                'label' => 'Inv.no',
+                                'value' => function ($data) {
+                                    if ($data->inbound_id > 0) {
+                                        return $data->invoice_no;
+                                    }else if($data->outbound_id > 0){
+                                        return $data->invoice_no;
+                                    }
+                                }
+                            ],
+                            ['attribute' => 'invoice_date', 'value' => function ($data) {
+                                return date('d-m-Y', strtotime($data->invoice_date));
                             }],
                             'transport_in_no',
-                            ['attribute' => 'transport_in_date','value'=>function($data){
-                                return date('d-m-Y',strtotime($data->transport_in_date));
+                            ['attribute' => 'transport_in_date', 'value' => function ($data) {
+                                return date('d-m-Y', strtotime($data->transport_in_date));
                             }],
-                            ['attribute' => 'sequence','value'=>function($data){
-                                return $data->sequence!=null?$data->sequence:0;
+                            ['attribute' => 'sequence', 'value' => function ($data) {
+                                return $data->sequence != null ? $data->sequence : 0;
                             }],
                             'permit_no',
-                            ['attribute' => 'permit_date','value'=>function($data){
-                                return date('d-m-Y',strtotime($data->permit_date));
+                            ['attribute' => 'permit_date', 'value' => function ($data) {
+                                return date('d-m-Y', strtotime($data->permit_date));
                             }],
                             'kno_no_in',
-                            ['attribute' => 'kno_in_date','value'=>function($data){
-                                return date('d-m-Y',strtotime($data->kno_in_date));
+                            ['attribute' => 'kno_in_date', 'value' => function ($data) {
+                                return date('d-m-Y', strtotime($data->kno_in_date));
                             }],
                             'usd_rate',
                             'thb_amount',
@@ -109,18 +120,19 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="row">
                 <div class="col-lg-9">
                     <div class="form-inline">
-                        <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+                        <?php echo $this->render('_search', ['model' => $searchModel]); ?>
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="pull-right">
-                        <form id="form-perpage" class="form-inline" action="<?=Url::to(['productstock/index'],true)?>" method="post">
+                        <form id="form-perpage" class="form-inline"
+                              action="<?= Url::to(['productstock/index'], true) ?>" method="post">
                             <div class="form-group">
                                 <label>แสดง </label>
                                 <select class="form-control" name="perpage" id="perpage">
-                                    <option value="20" <?=$perpage=='20'?'selected':''?>>20</option>
-                                    <option value="50" <?=$perpage=='50'?'selected':''?> >50</option>
-                                    <option value="100" <?=$perpage=='100'?'selected':''?>>100</option>
+                                    <option value="20" <?= $perpage == '20' ? 'selected' : '' ?>>20</option>
+                                    <option value="50" <?= $perpage == '50' ? 'selected' : '' ?> >50</option>
+                                    <option value="100" <?= $perpage == '100' ? 'selected' : '' ?>>100</option>
                                 </select>
                                 <label> รายการ</label>
                             </div>
@@ -128,102 +140,102 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
                 </div>
             </div>
-            <div class="table-responsive" >
-    <?php Pjax::begin(); ?>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+            <div class="table-responsive">
+                <?php Pjax::begin(); ?>
+                <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        // 'filterModel' => $searchModel,
-        'emptyCell'=>'-',
-        'formatter' => ['class' => 'yii\i18n\Formatter','nullDisplay' => ''],
-        'layout'=>"{items}\n{summary}\n<div class='text-left'>{pager}</div>",
-        'summary' => "แสดง {begin} - {end} ของทั้งหมด {totalCount} รายการ",
-        'showOnEmpty'=>false,
-        'tableOptions' => ['class' => 'table table-hover table-condensed'],
-        'options' => ['style' => 'white-space:nowrap; font-size: 14px'],
-        'emptyText' => '<br /><div style="color: red;align: center;"> <b>ไม่พบรายการไดๆ</b></div>',
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    // 'filterModel' => $searchModel,
+                    'emptyCell' => '-',
+                    'formatter' => ['class' => 'yii\i18n\Formatter', 'nullDisplay' => ''],
+                    'layout' => "{items}\n{summary}\n<div class='text-left'>{pager}</div>",
+                    'summary' => "แสดง {begin} - {end} ของทั้งหมด {totalCount} รายการ",
+                    'showOnEmpty' => false,
+                    'tableOptions' => ['class' => 'table table-hover table-condensed'],
+                    'options' => ['style' => 'white-space:nowrap; font-size: 14px'],
+                    'emptyText' => '<br /><div style="color: red;align: center;"> <b>ไม่พบรายการไดๆ</b></div>',
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
 
-           // 'id',
-            ['attribute' => 'product_id','value'=>function($data){
-                return \backend\models\Product::findCode($data->product_id);
-            }],
-            ['attribute' => 'product_id','format'=> 'html','value'=>function($data){
-                return '<a href="'.Url::to(['product/view','id'=>$data->product_id],true).'">'.\backend\models\Product::findName($data->product_id).'</a>';
-            }],
-            ['attribute' => 'warehouse_id','value'=>function($data){
-                return \backend\models\Warehouse::findWarehousename($data->warehouse_id);
-            }],
-            [
-                'attribute' => 'qty',
-                'headerOptions' => ['style'=>'background-color: green;color: white'],
-                'contentOptions' => ['style'=>'background-color: green;color: white;text-align: right'],
-                'value'=>function($data){
-                    if ($data->qty < 0) {
-                        return 0;
-                    } else {
-                        return $data->qty != null ? $data->qty : 0;
-                    }
+                        // 'id',
+                        ['attribute' => 'product_id', 'value' => function ($data) {
+                            return \backend\models\Product::findCode($data->product_id);
+                        }],
+                        ['attribute' => 'product_id', 'format' => 'html', 'value' => function ($data) {
+                            return '<a href="' . Url::to(['product/view', 'id' => $data->product_id], true) . '">' . \backend\models\Product::findName($data->product_id) . '</a>';
+                        }],
+                        ['attribute' => 'warehouse_id', 'value' => function ($data) {
+                            return \backend\models\Warehouse::findWarehousename($data->warehouse_id);
+                        }],
+                        [
+                            'attribute' => 'qty',
+                            'headerOptions' => ['style' => 'background-color: green;color: white'],
+                            'contentOptions' => ['style' => 'background-color: green;color: white;text-align: right'],
+                            'value' => function ($data) {
+                                if ($data->qty < 0) {
+                                    return 0;
+                                } else {
+                                    return $data->qty != null ? $data->qty : 0;
+                                }
 
-            }],
-            [
-                'attribute' => 'out_qty',
-                'headerOptions' => ['style'=>'background-color: orange;color: white'],
-                'contentOptions' => ['style'=>'background-color: orange;color: white;text-align: right'],
-                'value'=>function($data){
-                    if ($data->qty > 0) {
-                        return 0;
-                    } else {
-                        return $data->out_qty!=null?($data->out_qty * -1):0;
-                    }
-                }],
-            'invoice_no',
-            ['attribute' => 'invoice_date','value'=>function($data){
-               return date('Y',strtotime($data->invoice_date)) == 1970?'': date('d-m-Y',strtotime($data->invoice_date));
-            }],
-            'transport_in_no',
-            ['attribute' => 'transport_in_date','value'=>function($data){
-                return date('Y',strtotime($data->transport_in_date)) == 1970?'':date('d-m-Y',strtotime($data->transport_in_date));
-            }],
-            ['attribute' => 'sequence','value'=>function($data){
-                return $data->sequence!=null?$data->sequence:0;
-            }],
-            'permit_no',
-            ['attribute' => 'permit_date','value'=>function($data){
-                return date('Y',strtotime($data->permit_date)) == 1970?'':date('d-m-Y',strtotime($data->permit_date));
-            }],
-            'kno_no_in',
-            ['attribute' => 'kno_in_date','value'=>function($data){
-                return date('Y',strtotime($data->kno_in_date)) == 1970?'':date('d-m-Y',strtotime($data->kno_in_date));
-            }],
-            'usd_rate',
+                            }],
+                        [
+                            'attribute' => 'out_qty',
+                            'headerOptions' => ['style' => 'background-color: orange;color: white'],
+                            'contentOptions' => ['style' => 'background-color: orange;color: white;text-align: right'],
+                            'value' => function ($data) {
+                                if ($data->qty > 0) {
+                                    return 0;
+                                } else {
+                                    return $data->out_qty != null ? ($data->out_qty * -1) : 0;
+                                }
+                            }],
+                        'invoice_no',
+                        ['attribute' => 'invoice_date', 'value' => function ($data) {
+                            return date('Y', strtotime($data->invoice_date)) == 1970 ? '' : date('d-m-Y', strtotime($data->invoice_date));
+                        }],
+                        'transport_in_no',
+                        ['attribute' => 'transport_in_date', 'value' => function ($data) {
+                            return date('Y', strtotime($data->transport_in_date)) == 1970 ? '' : date('d-m-Y', strtotime($data->transport_in_date));
+                        }],
+                        ['attribute' => 'sequence', 'value' => function ($data) {
+                            return $data->sequence != null ? $data->sequence : 0;
+                        }],
+                        'permit_no',
+                        ['attribute' => 'permit_date', 'value' => function ($data) {
+                            return date('Y', strtotime($data->permit_date)) == 1970 ? '' : date('d-m-Y', strtotime($data->permit_date));
+                        }],
+                        'kno_no_in',
+                        ['attribute' => 'kno_in_date', 'value' => function ($data) {
+                            return date('Y', strtotime($data->kno_in_date)) == 1970 ? '' : date('d-m-Y', strtotime($data->kno_in_date));
+                        }],
+                        'usd_rate',
 //            'thb_amount',
-            //'status',
-            //'created_at',
-            //'updated_at',
-            //'created_by',
-            //'updated_by',
+                        //'status',
+                        //'created_at',
+                        //'updated_at',
+                        //'created_by',
+                        //'updated_by',
 
 //            ['class' => 'yii\grid\ActionColumn'],
-        ],
-        'condensed' => false,
-        'responsive' => false,
-        'containerOptions' => ['style'=>'overflow-y: '],
-        'resizableColumnsOptions' => ['resizeFromBody' => true],
-        'hover' => true,
+                    ],
+                    'condensed' => false,
+                    'responsive' => false,
+                    'containerOptions' => ['style' => 'overflow-y: '],
+                    'resizableColumnsOptions' => ['resizeFromBody' => true],
+                    'hover' => true,
 
 
-    ]); ?>
+                ]); ?>
             </div>
-    <?php Pjax::end(); ?>
+            <?php Pjax::end(); ?>
 
-</div>
+        </div>
     </div>
 </div>
 <?php
-$js=<<<JS
+$js = <<<JS
  $(function() {
     $("#perpage").change(function(){
             $("#form-perpage").submit();
@@ -231,6 +243,6 @@ $js=<<<JS
  });
 JS;
 
-$this->registerJs($js,static::POS_END);
+$this->registerJs($js, static::POS_END);
 
 ?>
