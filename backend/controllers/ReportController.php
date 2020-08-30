f01eea5ff9a5c093f9f68bb67ee7777d65b68aa2
tarlektarnoy <niran_tarlek@hotmail.com>
2020-08-30 12:39:22 +0700

    'update'

--- backend/controllers/ReportController.php
+++ backend/controllers/ReportController.php
@@ -262,9 +262,9 @@ class ReportController extends Controller
             $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
             if($selected_view_type == 1){
-                $dataProvider->query->andFilterWhere(['>=','amount','total_amount']);
+                $dataProvider->query->andFilterWhere(['>=','amount','total_amounts']);
             }else if($selected_view_type == 2){
-                $dataProvider->query->andFilterWhere(['<','amount','total_amount']);
+                $dataProvider->query->andFilterWhere(['<','amount','total_amounts']);
             }
             return $this->render('_arsummary', [
                 'dataProvider' => $dataProvider,
